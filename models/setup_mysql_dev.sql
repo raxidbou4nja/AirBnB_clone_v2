bf8b22d481383ec72f56a2e5d5018bef074365de
raxidbou4nja <rachid.boughnja@gmail.com>
2023-12-18 16:08:37 -0800

    add file of Mysql SetUp

--- models/setup_mysql_dev.sql
+++ models/setup_mysql_dev.sql
@@ -1,11 +1,11 @@
--- Create or use the database hbnb_dev_db
+-- create or use the database "hbnb_dev_db"
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create or use the user hbnb_dev
+-- greate or use the user "hbnb_dev"
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant privileges to hbnb_dev on hbnb_dev_db
+-- grant privileges to "hbnb_dev" on DataBase "hbnb_dev_db"
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on performance_schema to hbnb_dev
+-- grant SELECT privilege on performance_schema to "hbnb_dev"
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
