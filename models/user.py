2b090ec51062fb0e4f69dcd0e48d38d684f73e2e
derfal <sohail.bounnite@gmail.com>
2023-12-20 20:37:41 +0100

    fixing more errors user

--- models/user.py
+++ models/user.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    if (storage_engine == 'db'):
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -24,7 +24,3 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
