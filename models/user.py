e3a327ad2fd6df17dc08e63991712b02b77c707e
derfal <sohail.bounnite@gmail.com>
2023-12-20 22:07:50 +0100

    2nd attempt

--- models/user.py
+++ models/user.py
@@ -11,16 +11,10 @@ storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if (storage_engine == 'db'):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
