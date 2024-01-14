9c2a8758558e9c40663b45ec6e7b30111b1b1530
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-14 14:59:31 -0800

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,67 +1,59 @@
 #!/usr/bin/python3
 """
-Pack static content and deploy on servers
+    pack static content and deploy on server
 """
-
 import time
-from fabric.api import local, run, put, env
+from fabric.context_managers import cd
+from fabric.api import local, get, put, reboot, run, sudo, env
+import os.path
 
 env.hosts = ['100.26.227.175', '54.166.43.77']
 
 
 def do_pack():
-    """Pack the web_static content"""
+    """ pack my static"""
     try:
-        current_time = time.strftime("%Y%m%d%H%M%S", time.gmtime())
-        archive_path = "versions/web_static_{}.tgz".format(current_time)
-        local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except Exception as e:
+        if not os.path.exists('versions'):
+            l = local("mkdir -p versions")
+        n = "versions/web_static_{}.tgz".\
+            format(time.strftime("%Y%m%d%H%M%S", time.gmtime()))
+        o = local("tar -cvzf {} web_static".format(n))
+        # x = local("mv {} versions".format(n))
+        # p = local("pwd {}".format(n))
+        # return 'versions/{}'.format(n)
+        return n
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """Deploy the archive to the web servers"""
-    if not archive_path:
-        return False
-
+    """ deploy my archive tgz into my servers """
     try:
         put(archive_path, '/tmp/')
-
-        run('mkdir -p /data/web_static/releases/{}/'
-            .format(archive_path[9:-4]))
-
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_path[9:], archive_path[9:-4]))
-
+        c1 = 'mkdir -p /data/web_static/releases/{}/'
+        run(c1.format(archive_path[9:-4]))
+        c2 = 'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run(c2.format(archive_path[9:], archive_path[9:-4]))
         run('rm /tmp/{}'.format(archive_path[9:]))
-
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'
-            .format(archive_path[9:-4], archive_path[9:-4]))
-
-        run('rm -rf /data/web_static/releases/{}/web_static/'
-            .format(archive_path[9:-4]))
-
+        c3 = 'mv /data/web_static/releases/{}/web_static/* \
+              /data/web_static/releases/{}/'
+        run(c3.format(archive_path[9:-4], archive_path[9:-4]))
+        c4 = 'rm -rf  /data/web_static/releases/{}/web_static/'
+        run(c4.format(archive_path[9:-4]))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_path[9:-4]))
-
+        c5 = 'ln -s /data/web_static/releases/{}/ {}'
+        run(c5.format(archive_path[9:-4], '/data/web_static/current'))
         return True
-    except Exception as e:
-        print("Error:", e)
+    except:
         return False
 
 
-archive_path = do_pack()
+path = do_pack()
 
 
 def deploy():
-    """Pack and deploy"""
-
-    return do_deploy(archive_path) if archive_path else False
-
-
-if __name__ == "__main__":
-    deploy()
+    """ pack and deploy """
+    if path:
+        return do_deploy(path)
+    else:
+        return False
