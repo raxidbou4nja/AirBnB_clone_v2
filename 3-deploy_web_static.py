db5d97a90bc12e904d264c5f6f109a543a98ae4f
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-14 15:03:27 -0800

    add new fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,59 +1,50 @@
 #!/usr/bin/python3
-"""
-    pack static content and deploy on server
-"""
-import time
-from fabric.context_managers import cd
-from fabric.api import local, get, put, reboot, run, sudo, env
-import os.path
+""" Function that deploys """
 
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
 env.hosts = ['100.26.227.175', '54.166.43.77']
 
 
 def do_pack():
-    """ pack my static"""
+    """Creates a tgz archive using fabric"""
     try:
-        if not os.path.exists('versions'):
-            l = local("mkdir -p versions")
-        n = "versions/web_static_{}.tgz".\
-            format(time.strftime("%Y%m%d%H%M%S", time.gmtime()))
-        o = local("tar -cvzf {} web_static".format(n))
-        # x = local("mv {} versions".format(n))
-        # p = local("pwd {}".format(n))
-        # return 'versions/{}'.format(n)
-        return n
-    except:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        filename = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception as ex:
         return None
 
 
 def do_deploy(archive_path):
-    """ deploy my archive tgz into my servers """
-    try:
-        put(archive_path, '/tmp/')
-        c1 = 'mkdir -p /data/web_static/releases/{}/'
-        run(c1.format(archive_path[9:-4]))
-        c2 = 'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-        run(c2.format(archive_path[9:], archive_path[9:-4]))
-        run('rm /tmp/{}'.format(archive_path[9:]))
-        c3 = 'mv /data/web_static/releases/{}/web_static/* \
-              /data/web_static/releases/{}/'
-        run(c3.format(archive_path[9:-4], archive_path[9:-4]))
-        c4 = 'rm -rf  /data/web_static/releases/{}/web_static/'
-        run(c4.format(archive_path[9:-4]))
-        run('rm -rf /data/web_static/current')
-        c5 = 'ln -s /data/web_static/releases/{}/ {}'
-        run(c5.format(archive_path[9:-4], '/data/web_static/current'))
-        return True
-    except:
+    """deploy web static with fabric"""
+    if exists(archive_path) is False:
         return False
 
-
-path = do_pack()
+    try:
+        filename = archive_path.split("/")[-1]
+        no_excep = filename.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}{}/'.format(path, no_excep))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        return True
+    except BaseException:
+        return False
 
 
 def deploy():
-    """ pack and deploy """
-    if path:
-        return do_deploy(path)
-    else:
+    """ do path an do deploy"""
+    archive_path = do_pack()
+    if archive_path is None:
         return False
+    return do_deploy(archive_path)
