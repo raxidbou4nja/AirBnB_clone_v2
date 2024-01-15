19d787dee4326597c03cbff5a479dac202f8cd28
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-14 16:22:56 -0800

    add new code

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,68 +1,87 @@
-# nginx_setup.pp
+# Puppet for setup
+
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
 package { 'nginx':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'apt'
 }
 
-file { '/data':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
+-> file { '/data':
+  ensure  => 'directory'
 }
 
-file { '/data/web_static':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/shared':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html><head><title>Test Page</title></head><body>Hello, this is a test page!</body></html>',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
-file { '/data/web_static/current':
-  ensure  => link,
-  target  => '/data/web_static/releases/test/',
-  force   => true,
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-file_line { 'nginx_config':
-  path   => '/etc/nginx/sites-available/default',
-  line   => '  location /hbnb_static/ {',
-  match  => '^server {',
-  after  => '  }',
-  notify => Service['nginx'],
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['nginx_config'],
+file { '/var/www':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+}
+
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
